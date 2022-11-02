719b4e5c0f89057f8dd016fefa7ff55f2bb16004
Dmitry-Gavrilov <91532052+Dmitry-Gavrilov@users.noreply.github.com>
2022-11-02 20:08:05 +1000

    redacted

--- src/js/characters.js
+++ src/js/characters.js
@@ -1,11 +1,9 @@
 export default class Characters {
-  constructor(name, type, attack, defence) {
+  constructor(name, type) {
     this.name = name;
     this.type = type;
     this.health = 100;
     this.level = 1;
-    this.attack = attack;
-    this.defence = defence;
     const charTypes = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
     if (typeof name !== 'string' || this.name.length > 10 || this.name.length < 2) {
       throw new Error('Длина имени должна быть от 2 до 10 символов');
